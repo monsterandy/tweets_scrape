6e848020b7cc5dc6f7cbf8e0b9777b303b69bf3b
anooshaseelm <anooshaseelam15@gmail.com>
2021-06-21 19:55:37 -0400

    added Tesseract OCR file

---
+++ tesseract_ocr.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import pytesseract
+
+custom_config = r"--oem 3 --psm 11 -c tessedit_char_whitelist= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ '"
+pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
+
+
+PERFORM_THRESHOLDING = True
+
+
+def TextRecognition(image_path):
+    img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    if PERFORM_THRESHOLDING:
+        _, img = cv2.threshold(cv2.cvtColor(
+            img, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        img = cv2.bitwise_not(img)
+        text = pytesseract.image_to_string(
+            img, lang='eng', config=custom_config)
+        # text = text.replace('\n', ',')
+        return text
