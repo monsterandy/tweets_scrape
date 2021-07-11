290084a2ac83d3bf06ba905ac2d3ed009b8bdfc3
monsterandy <philoround@gmail.com>
2021-07-11 02:22:35 -0400

    Update scripts for CCR computing

---
+++ ccr_scripts/job_immigration_08.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#SBATCH --partition=general-compute --qos=general-compute
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --constraint=IB
+#SBATCH --mem=2000
+# Memory per node specification is in MB. It is optional. 
+# The default limit is 3000MB per core.
+#SBATCH --job-name="08_immigration"
+#SBATCH --output=../ccr_outputs/job_immigration_08.out
+#SBATCH --mail-user=zheyuanm@buffalo.edu
+#SBATCH --mail-type=ALL
+
+/usr/bin/hostname
+which python
+
+echo "SLURM_JOBID="$SLURM_JOBID
+echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
+echo "SLURM_NNODES"=$SLURM_NNODES
+echo "SLURMTMPDIR="$SLURMTMPDIR
+echo "working directory = "$SLURM_SUBMIT_DIR
+
+module use /projects/academic/hongxinh/modulefiles
+module load python/my-conda
+source /projects/academic/hongxinh/anaconda/etc/profile.d/conda.sh
+conda activate tweets
+which python
+cd ..
+pwd
+
+echo "Job start!"
+python autocommand.py -t immigration -s 2020-08-01 -e 2020-08-31
+python scrape_tweets.py -n immigration_08
+python image_extract.py -n immigration_08
+
+module unload python/my-conda
+which python
+echo "Job end!"
