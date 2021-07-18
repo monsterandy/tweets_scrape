a8d32d666d03d561dc1c40a1e8b607fe81421d2d
anooshaseelm <anooshaseelam15@gmail.com>
2021-07-17 22:02:43 -0400

    Text from Google OCR

---
+++ GoogleCloudAPI.py
@@ -0,0 +1,29 @@
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image, image_context={"language_hints": ["en"]},)
+    texts = response.text_annotations
+    # print('Texts:')
+    x = None
+    for text in texts:
+        x = '\n"{}"'.format(text.description)
+        break
+        # vertices = (['({},{})'.format(vertex.x, vertex.y)
+        #             for vertex in text.bounding_poly.vertices])
+
+        # print('bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+    return x
